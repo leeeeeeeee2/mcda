60963d7af97124f18ea249dc8cf1ae529985f14d
kiziebar <kiziub@gmail.com>
2021-08-02 10:01:25 +0200

    Add correlation_matrix

--- pymcdm/correlations.py
+++ pymcdm/correlations.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2020 Andrii Shekhovtsov
+# Copyright (c) 2021 Bartłomiej Kizielewicz
 
 import numpy as np
 from itertools import permutations
@@ -9,7 +10,8 @@ __all__ = [
     'weighted_spearman',
     'rank_similarity_coef',
     'kendall_tau',
-    'goodman_kruskal_gamma'
+    'goodman_kruskal_gamma',
+    'correlation_matrix'
 ]
 
 def _cov(x, y):
@@ -147,3 +149,33 @@ Returns
         if sign:
             den += 1
     return num / float(den)
+
+
+def correlation_matrix(rankings, method, columns=False):
+    """Creates a correlation matrix for given vectors from the numpy array.
+
+    Parameters
+    ----------
+        rankings : ndarray
+            Vectors for which the correlation matrix is to be calculated.
+
+        method : callable
+            Function to calculate the correlation matrix.
+
+        columns: bool
+            If the column value is set to true then the correlation matrix will be calculated for the columns.
+            Otherwise the matrix will be calculated for the rows.
+
+    Returns
+    -------
+        ndarray
+            Correlation between two rankings vectors.
+    """
+    if columns:
+        rankings = rankings.T
+    n = rankings.shape[0]
+    corr = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            corr[i, j] = method(rankings[i], rankings[j])
+    return corr
