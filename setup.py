4008168961b73258324c3a3b8f9090643872e4ff
kiziebar <kiziub@gmail.com>
2021-08-14 14:51:45 +0200

    Setup update to 1.0.4 version

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pymcdm",
-    version="1.0.3.post2",
-    author="Andrii Shekhovtsov",
-    author_email="andrii-shekhovtsov@zut.edu.pl",
+    version="1.0.4",
+    author="Andrii Shekhovtsov, Bartłomiej Kizielewicz",
+    author_email="andrii-shekhovtsov@zut.edu.pl, bartlomiej-kizielewicz@zut.edu.pl",
     description="Python library for Multi-Criteria Decision-Making",
     long_description=long_description,
     long_description_content_type="text/markdown",
