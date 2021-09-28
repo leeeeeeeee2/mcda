:bug: standard_deviation_weights fix ddof

pymcdm/weights.py
@@ -87,7 +87,7 @@ def standard_deviation_weights(matrix, *args, **kwargs):
         ndarray
             Vector of weights.
     """
-    std = np.std(matrix, axis=0, ddof=1)
+    std = np.std(matrix, axis=0)
     return std / np.sum(std)
 
 